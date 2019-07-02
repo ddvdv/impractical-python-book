5a180e00de995dbf13076e985c884d97a514b386
unknown <david@dvdv.eu>
2019-07-02 23:37:44 +0200

    palindromes step1

---
+++ chap2/palindrom.py
@@ -0,0 +1,13 @@
+"""Work with palindrom and palingram."""
+
+import load_dictionary
+
+dictionnary = load_dictionary.load('2of4brif.txt')
+palindromes = []
+
+for word in dictionnary:
+    if word[:] == word[::-1]:
+        palindromes.append(word)
+
+print(*palindromes, sep="\n")
+    
